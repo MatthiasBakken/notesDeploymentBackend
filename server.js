decefd3d7dba7845315250a71fe3ef627eb0f5de
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-28 13:03:28 -0700

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const port = process.env.PORT || 5000;
 const setupMiddleware = require('./_config/middleware');
 const db = require('./_config/db');
 const setupRoutes = require('./_config/routes');
@@ -12,8 +12,8 @@ setupRoutes(server);
 db.connectTo()
   .then(() => {
     console.log('\n... API Connected to lambdaNotes Database ...\n');
-    server.listen(5000, () =>
-      console.log('\n=== API running on port 5000 ===\n')
+    server.listen(port, () =>
+      console.log(`\n=== API running on port ${port} ===\n`)
     );
   })
   .catch(err => {
