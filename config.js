562ec7b04c23dbbb321c403b1d95989a6914df0a
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-28 12:27:11 -0700

    make it better

---
+++ config.js
@@ -0,0 +1,7 @@
+const origin = "http://localhost:3000";
+
+const dbPath = "mongodb://localhost/lambdaNotes"
+
+const mLabDB = "mongodb://lambdanotes:lambdanotes1@ds163300.mlab.com:63300/notes321"
+
+module.exports = { origin, dbPath, mLabDB };
