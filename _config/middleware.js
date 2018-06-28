c90cebed2fb912173fb0da68c3f31c905e42c3d0
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-28 12:57:31 -0700

    make it better

--- _config/middleware.js
+++ _config/middleware.js
@@ -3,12 +3,23 @@ const helmet = require('helmet');
 const cors = require('cors');
 const config = require('../config');
 const corsOptions = {
-  origin: "*",
-  credentials: true
+  origin: '*',
+  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 };
 
 module.exports = function (server) {
   server.use(helmet());
   server.use(express.json());
+  // Begin code for cross-site allowances -------------------------------------
+  server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Cross Site Allowance
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
   server.use(cors(corsOptions));
 };
